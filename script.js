555377122135c29a6c35d5cec1b9e3a2585db709
NoUs3rName007 <130101224+NoUs3rName007@users.noreply.github.com>
2026-03-10 21:14:49 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Fixing the order of destructuring for CSV columns
-const csvData = csvRows.map(row => {
-    const [date, min_temp, max_temp] = row; // swapped here
-    return { date, min_temp, max_temp };
-});
